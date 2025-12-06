bd4fd2f9e5827a6f26b519c2ca998019af73d0aa
Hiranmayi08 <5670hiranmayi@gmail.com>
2025-12-06 10:04:34 +0530

    Add files via upload

---
+++ pointer arr.c
@@ -0,0 +1,23 @@
+//write a progarm for reading elements using a pointer into an array and display the values using the array.
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i;
+    int *ptr;
+    printf("enter number of elements:");
+    scanf("%d", &n);
+    ptr=arr; //pointer points to the beginning of array
+    //reading elements using pointer
+    printf("enter %d elements:\n",n);
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",(ptr+i)); //reading using pointer arithemetic
+    }
+    // displaying elements using array notation
+    printf("\narray elements are:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d", arr[i]); //displaying using array
+    }
+    return 0;
+}
