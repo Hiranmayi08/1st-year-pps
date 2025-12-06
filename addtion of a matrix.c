8496b241f41e5a871cba2e64f500cac20a459639
Hiranmayi08 <5670hiranmayi@gmail.com>
2025-12-06 09:57:40 +0530

    Add files via upload

---
+++ addtion of a matrix.c
@@ -0,0 +1,41 @@
+#include<stdio.c>
+int main()
+{
+    int m,n,i,j;
+    printf("enter the number of rows and coloumns of the matrix:");
+    scanf("%d%d", &m,&n);
+    int a[m][n],b[m][n],c[m][n];
+printf("enter the elements of a matrix A:\n");
+for (i=0;i<m;i++)
+{
+    for (j=0;j<n;j++)
+    {
+    scanf("%d", &a[i][j]);
+    }
+}
+printf("enter the elements of matrix B:\n");
+for (i=0;i<m;i++)
+{
+    for (j=0;j<n;j++)
+    {
+        scanf("%d", &b[i][j]);
+    }
+}
+for (i=0;i<m;i++)
+{
+    for (j=0;j<n;j++)
+    {
+    c[i][j]=a[i][j]+b[i][j];
+    }
+}
+printf("the sum of two matrices is:\n");
+for(i=0;i<m;i++)
+{
+    for (j=0;j<n;j++)
+    {
+        printf("%d", c[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
